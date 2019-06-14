ac8631eb9bd59cd20ea65e7f765b420550fb902a
Chris Woodward <cwoodward@narrative.io>
2019-06-14 13:36:44 -0400

    Moved User class ton Auth module

---
+++ src/app/modules/auth/user.class.ts
@@ -0,0 +1,13 @@
+export class User {
+    id: number;
+    name: string;
+    email: string;
+    hashedPassword: string;
+    salt: string;
+    createdAt: string;
+    updatedAt: string;
+
+    constructor() {
+        
+    }
+}
