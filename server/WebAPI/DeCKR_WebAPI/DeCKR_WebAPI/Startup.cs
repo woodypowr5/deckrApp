c79710eaec1949469694bbab1123f7b240f3374e
krishnteja <krishnteja@gmail.com>
2019-06-14 20:23:16 -0700

    Adding Server folder

---
+++ server/WebAPI/DeCKR_WebAPI/DeCKR_WebAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeCKR_WebAPI.Startup))]
+namespace DeCKR_WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
