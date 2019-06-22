920916f1003df2a1ad1cda2ae2099a70674c3d31
Chris Woodward <cwoodward@narrative.io>
2019-06-21 23:18:47 -0700

    Trainings render in TrainingComponent. Added element margin styles

--- client/src/app/modules/training/components/training/training.component.ts
+++ client/src/app/modules/training/components/training/training.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { TrainingService } from '../../training.service';
+import { Training } from 'src/app/shared/types/training.class';
 
 @Component({
-  selector: 'app-training',
-  templateUrl: './training.component.html',
-  styleUrls: ['./training.component.scss']
+	selector: 'app-training',
+	templateUrl: './training.component.html',
+	styleUrls: ['./training.component.scss']
 })
 export class TrainingComponent implements OnInit {
+	private trainings: Training[] = [];
+	
+  	constructor(
+	  	private trainingService: TrainingService
+  	) { 
+		this.trainingService.trainingsChanged.subscribe( (newTrainings: Training[]) => {
+			this.trainings = newTrainings;
+		});		
+	}
 
-  constructor(
-	  private trainingService: TrainingService
-  ) { }
-
-  ngOnInit() {
-  }
-
+  	ngOnInit() {
+  
+	}
 }
--- client/src/app/modules/training/training.module.ts
+++ client/src/app/modules/training/training.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { TrainingComponent } from './components/training/training.component';
 import { TrainingRoutingModule } from './training-routing.module';
+import { SharedModule } from 'src/app/shared/modules/shared.module';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		TrainingRoutingModule
+		TrainingRoutingModule,
+		SharedModule
 	],
 	declarations: [
 		TrainingComponent
