c9769b8f7c1dc4000e9d2edcfc97025d00a19761
Chris Woodward <cwoodward@narrative.io>
2019-07-12 22:24:28 -0700

    populated userProgress and moduleProgress fixtures

---
+++ client/src/app/shared/data/fixtures/moduleProgress.ts
@@ -0,0 +1,40 @@
+import { ModuleProgress } from '../../types/module-progress';
+
+export const moduleProgressFixture: ModuleProgress[] = [
+	{
+		completedTime: 5,
+		moduleID: 1,
+		moduleName: "Registration",
+		totalTime: 5
+	},
+	{
+		completedTime: 5,
+		moduleID: 2,
+		moduleName: "Login",
+		totalTime: 5
+	},
+	{
+		completedTime: 5,
+		moduleID: 3,
+		moduleName: "User Home",
+		totalTime: 5
+	},
+	{
+		completedTime: 0,
+		moduleID: 4,
+		moduleName: "Trainings",
+		totalTime: 1280
+	},
+	{
+		completedTime: 0,
+		moduleID: 5,
+		moduleName: "Contracts",
+		totalTime: 0
+	},
+	{
+		completedTime: 0,
+		moduleID: 6,
+		moduleName: "Security Groups",
+		totalTime: 4
+	}
+]
---
+++ client/src/app/shared/data/fixtures/userProgress.ts
@@ -0,0 +1,22 @@
+import { UserProgress } from '../../types/user-progress';
+
+export const userProgressFixture: UserProgress[] = [
+	{
+		completedTime: 15,
+		employeeID: 1,
+		name: "Test IT User",
+		totalTime: 1299
+	},
+	{
+		completedTime: 195,
+		employeeID: 2,
+		name: "Test Personell User",
+		totalTime: 800
+	},
+	{
+		completedTime: 15,
+		employeeID: 3,
+		name: "Test Sales User",
+		totalTime: 300
+	}	
+]
