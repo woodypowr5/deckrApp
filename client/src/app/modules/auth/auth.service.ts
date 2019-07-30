bd41db96fa7a2fcb3871d5fb6412b1c427a87d4e
Chris Woodward <cwoodward@narrative.io>
2019-07-30 15:06:13 -0700

    Solved authentication nightmare

--- client/src/app/modules/auth/auth.service.ts
+++ client/src/app/modules/auth/auth.service.ts
@@ -6,11 +6,33 @@ import { UsersApiService } from 'src/app/shared/api/users-api.service';
 import { AuthApiService } from 'src/app/shared/api/auth-api.service';
 import { JobRolesApiService } from 'src/app/shared/api/job-roles-api.service';
 import { JobRole } from 'src/app/shared/types/job-role';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import * as jwt_decode from "jwt-decode";
+import { ApiUrls } from 'src/app/shared/api/api-urls';
+import { map } from 'rxjs/operators';
+
+interface AuthUser {
+	Email: string;
+	Name: string;
+	JobRole: string;
+	password: string;
+	confirmpassword: string;	
+};
+
+interface AuthLoginRequest {
+	Grant_type: string,
+	Username: string,
+	Password: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+	private apiUrl: string = ApiUrls.base + ApiUrls.auth.base;
+	private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+	private encodedHeaders = new  HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded'});
 	private loggedInUser: User;
 	loggedInUserChanged: BehaviorSubject<User> = new BehaviorSubject(null);
 	private users: User[];
@@ -26,11 +48,13 @@ export class AuthService {
 		private router: Router,
 		private usersApi: UsersApiService,
 		private authApi: AuthApiService,
-		private jobRolesApi: JobRolesApiService
+		private jobRolesApi: JobRolesApiService,
+		private http: HttpClient,
+		private jwtHelper: JwtHelperService,
 	) { 
 		this.loggedInUserChanged.subscribe((user: User) => {
+			console.log(user)
 			this.loggedInUser = user;
-			console.log(user.id);
 			if (user && user !== null){
 				this.router.navigate(["home"]);
 				this.isAuthChanged.next(true);
@@ -53,12 +77,25 @@ export class AuthService {
 		this.authApi.loggedInUserChanged.subscribe((user: User) => {
 			this.loggedInUserChanged.next(user);
 		});
+		this.getLoggedInUser();
 	}
 
 	getUsers() {
 		this.usersApi.getUsers().subscribe(data => {
 			this.usersChanged.next(this.users = data);
 		})
+	}
+
+	getLoggedInUser(): void {
+		const token = this.authApi.getToken();
+		if (token) {
+			const parts = token.split(",");
+			const idPart = parts[4].replace(/['"]+/g, '');
+			const id = idPart.split(":")[1];
+			this.usersApi.getUserById(parseInt(id)).subscribe((data: User) =>{		
+				this.loggedInUserChanged.next(data);
+			});
+		}
 	}
 
 	getJobRoles() {
@@ -75,17 +112,13 @@ export class AuthService {
 	loginUser(email: string, password: string): Promise<any> {
 		return new Promise<any>((resolve, reject) => {
 			this.authApi.authenticate(email, password).subscribe(() => {
-				this.authApi.getLoggedInUser();
+				this.getLoggedInUser();
 				this.router.navigate(["/home"]);
 				resolve(null);
 			}, (error) => {
 				resolve(error);
 			});
 		});
-	}
-
-	registerUser(newUser: User): Observable<number> {
-		return this.authApi.registerUser(newUser);
 	}
 
 	getIsAdmin(): boolean {
@@ -101,5 +134,59 @@ export class AuthService {
 		this.loggedInUserChanged.next(null);
 		localStorage.removeItem('token');
 		this.router.navigate(["login"]);
+	}
+
+
+	getToken() {
+		return this.jwtHelper.tokenGetter();
+	}
+
+	makeAuthUser(user: User): any {
+		return {
+			Email: user.email,
+			Name: user.name,
+			JobRole: user.role,
+			password: user.hashedPassword,
+			confirmpassword: user.hashedPassword
+		}
+	}
+
+	registerUser(user: User): Observable<number> {
+		const url = this.apiUrl + "/register/";
+		return this.http.post<number>(url ,JSON.stringify(this.makeAuthUser(user)), { headers: this.headers });
+	}
+
+	authenticate(username: string, password: string): Observable<any> {
+		const url = "http://deckrwebapi.azurewebsites.net/token";
+		let headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+		let body = new URLSearchParams();
+		body.set('username', username);
+		body.set('password', password);
+		body.set('grant_type', "password");
+		body.set('client_id', "angular.client");
+		body.set('client_secret', "secret");
+		return this.http.post<any>(url, body.toString(), {
+		  headers: headers
+		}).pipe(
+		  map(jwt => {
+			if (jwt && jwt.access_token) {
+			  localStorage.setItem('token', JSON.stringify(jwt))
+			}
+		  })
+		);
+	}
+
+	getDecodedAccessToken(token: string): any {
+		try{
+			return jwt_decode(token);
+		}
+		catch(Error){
+			return undefined;
+		}
+	}
+	
+	isAuthenticated(): boolean {
+		const token = this.getToken();
+		if (token !== null && token !== undefined) {return true};
 	}
 }
